2c06bd0bce50df51cf46200e6504f768647bf6ef
ulyanovskk <mr1english4@gmail.com>
2021-10-06 20:36:32 +0300

    Just fixed random slider bug :| https://github.com/primefaces/primefaces/issues/4791

--- src/main/java/servlets/LoginFilter.java
+++ src/main/java/servlets/LoginFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Objects;
 
-@WebFilter(urlPatterns = "*")
+@WebFilter(urlPatterns = {"/table", "/app.xhtml", "/points"})
 public class LoginFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
@@ -20,9 +20,12 @@ public class LoginFilter extends HttpFilter {
             System.out.println("Redirect because no name was provided.");
             req.getRequestDispatcher("/index.xhtml").forward(req, res);
         }
-        else if (welcomeBean.getName().equals("") && req.getRequestURL().toString().contains("index")) {
-            System.out.println("Redirect because no name was provided.");
-            req.getRequestDispatcher("/index.xhtml").forward(req, res);
-        } else chain.doFilter(req, res);
+        else {
+            String request = req.getRequestURL().toString();
+            if (welcomeBean.getName().equals("") && !request.contains("index")) {
+                System.out.println("Redirect because no name was provided.");
+                req.getRequestDispatcher("/index.xhtml").forward(req, res);
+            } else chain.doFilter(req, res);
+        }
     }
 }
